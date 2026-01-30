0c22688b62749bd0786230ea5c509de44b91803e
João Luís <joaoluispedrosateles145@gmail.com>
2026-01-30 13:24:55 -0300

    Aula 05: Entendendo sobre Bibliotecas e Funções Nativas

---
+++ python/funcoes_nativas_bibliotecas.py
@@ -0,0 +1,17 @@
+import math
+import numpy as np
+from numpy import linspace
+from numpy import linalg as la
+
+print(type(2))
+
+print("Teste")
+
+print(math.sqrt(2))
+print(math.pi)
+
+print(np.sum([1, 5, 6, 190]))
+
+print(linspace(0, 1, 10))
+
+print(la.det)
